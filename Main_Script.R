78245878c90f9c7db75a054d441246f76aee44fb
Joshua Sodicoff <44811472+jsodicoff@users.noreply.github.com>
2022-02-23 13:38:27 -0500

    Added example of preprocessing function

--- Main_Script.R
+++ Main_Script.R
@@ -9,6 +9,7 @@ create.directories(region = "ORB")
 
 filenames = c(tenx_1 = "/nfs/turbo/umms-welchjd/BRAIN_initiative/iso_allen/AUD_rna_mat.RDS", smart = "/nfs/turbo/umms-welchjd/BRAIN_initiative/rna/zeng_smart/AUD_smart_mat.RDS", tenx_2 = "/nfs/turbo/umms-welchjd/akriebel/Brain_initiative/AUDv2/QC_AUD/QC_data/Huang_filtered_data_AUD.RDS", atac = "/nfs/turbo/umms-welchjd/akriebel/Brain_initiative/AUDv2/QC_AUD/QC_data/ATAC_filtered_data_AUD.RDS")
 apply_qc(filenames, region = "ORB", analysis_num = 1, qc_table_path = "/nfs/turbo/umms-welchjd/BRAIN_initiative/BICCN_integration_Analyses/Base_Reference_Files/QC_table.RDS", filepath = "/scratch/welchjd_root/welchjd0/akriebel/Test")
+object = preprocess_data("/nfs/turbo/umms-welchjd/BRAIN_initiative/BICCN_integration_Analyses", "ORB", 1, 1000)
 
 
 
