af8814da95718852629aa459f0a05f36f89f44ab
Joshua Sodicoff <44811472+jsodicoff@users.noreply.github.com>
2022-06-03 13:19:11 -0400

    Added functionality to subset spatial data for downstream analysis.

--- R/deconv.R
+++ R/deconv.R
@@ -1,7 +1,12 @@
 # Functions required for utilization of the DUMfound deconvolution pipeline on
 # fully annotated region atlases
 
-### TODO document
+#' @param filepath Path to directory within which the atlas structure was generated
+#' @param region The anatomical region with which the analysis is associated
+#' @param spatial.data.file A filepath to the spatial expression matrix
+#' @param coords An object or filepath to coordinates for the spatial samples
+#' @param spatial.data.name A string, the name of the spatial dataset
+
 save_spatial_data = function(filepath,
                         region,
                         spatial.data.file,
@@ -17,6 +22,149 @@ save_spatial_data = function(filepath,
     file.copy(coords, paste0(dir_new,"/",spatial.data.name,"_coords.RDS"))
   } else {
     saveRDS(coords, paste0(dir_new,"/",spatial.data.name,"_coords.RDS"))
+  }
+}
+
+#' Generate plots of the provided spatial data on the XY, YZ, and XZ planes in
+#' a reference grid to allow for indexing.
+#'
+#' @param filepath Path to directory within which the atlas structure was generated
+#' @param region A string corresponding to the name of an anatomical region
+#' @param spatial.data.name A string, the name of the spatial dataset
+#' @param display.plots A logical, corresponding with if to display requested plots upon
+#'    generation
+#'
+#' @return nothing
+#'
+#' @import ggplot2, cowplot, viridis
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#'
+#' }
+#'
+
+reference_3d_coordinates = function(filepath,
+                                    region,
+                                    spatial.data.name,
+                                    save.plots = FALSE){
+  
+  library(ggplot2)
+  library(cowplot)
+  coords = readRDS(paste0(filepath,"/",  region, "/", region,"_Deconvolution_Output/",spatial.data.name,"/",spatial.data.name,"_coords.RDS"))
+  
+  minmax = apply(coords, MARGIN = 2, function(x){range(x)})
+  ranges = apply(minmax, MARGIN = 2, function(y){return(y[2]-y[1])})
+  
+  p1 = ggplot(coords, aes_string(x = colnames(coords)[1], y = colnames(coords)[2]))+
+    geom_tile() +
+    coord_fixed(ratio = 1)+
+    geom_tile(aes(alpha = .1)) +
+    coord_fixed(ratio = 1) +
+    theme_minimal_grid() +
+    scale_y_continuous(minor_breaks = seq(minmax[1,2] , minmax[2,2], 1), breaks = seq(minmax[1,2] , minmax[2,2], 5)) +
+    scale_x_continuous(minor_breaks = seq(minmax[1,1] , minmax[2,1], 1), breaks = seq(minmax[1,1] , minmax[2,1], 5)) +
+    xlab(colnames(coords)[1]) +
+    ylab(colnames(coords)[2]) +
+    ggtitle(paste0("X-Y silhouette of ",region))+
+    theme(legend.position="none",
+          text = ggplot2::element_text(size = 5),
+          axis.text = ggplot2::element_text(size = 4),
+          plot.title = element_text(size=6))
+  p2 = ggplot(coords, aes_string(x = colnames(coords)[1], y = colnames(coords)[3])) +
+    geom_tile() +
+    coord_fixed(ratio = 1)+
+    geom_tile(aes(alpha = .1)) +
+    coord_fixed(ratio = 1) +
+    theme_minimal_grid() +
+    scale_y_continuous(minor_breaks = seq(minmax[1,3] , minmax[2,3], 1), breaks = seq(minmax[1,3] , minmax[2,3], 5)) +
+    scale_x_continuous(minor_breaks = seq(minmax[1,1] , minmax[2,1], 1), breaks = seq(minmax[1,1] , minmax[2,1], 5)) +
+    xlab(colnames(coords)[1]) +
+    ylab(colnames(coords)[3]) +
+    ggtitle(paste0("X-Z silhouette of ",region))+
+    theme(legend.position="none",
+          text = ggplot2::element_text(size = 5),
+          axis.text = ggplot2::element_text(size = 4),
+          plot.title = element_text(size=6))
+  p3 = ggplot(coords, aes_string(x = colnames(coords)[2], y = colnames(coords)[3])) +
+    geom_tile() +
+    coord_fixed(ratio = 1)+
+    geom_tile(aes(alpha = .1)) +
+    coord_fixed(ratio = 1) +
+    theme_minimal_grid() +
+    scale_y_continuous(minor_breaks = seq(minmax[1,3] , minmax[2,3], 1), breaks = seq(minmax[1,3] , minmax[2,3], 5)) +
+    scale_x_continuous(minor_breaks = seq(minmax[1,2] , minmax[2,2], 1), breaks = seq(minmax[1,2] , minmax[2,2], 5)) +
+    xlab(colnames(coords)[2]) +
+    ylab(colnames(coords)[3]) +
+    ggtitle(paste0("Y-Z silhouette of ",region))+
+    theme(legend.position="none",
+          text = ggplot2::element_text(size = 5),
+          axis.text = ggplot2::element_text(size = 4),
+          plot.title = element_text(size=6))
+  print(p1)
+  print(p2)
+  print(p3)
+  if(save.plots){
+    if(!dir.exists(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots"))){
+      dir.create(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/plots"))
+    }
+    ggplot2::ggsave(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots/x_y_coord_reference.PNG"),
+                    p1,
+                    width = 500,
+                    height = 500,
+                    units = "px")
+    ggplot2::ggsave(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots/x_z_coord_reference.PNG"),
+                    p2,
+                    width = 500,
+                    height = 500,
+                    units = "px")
+    ggplot2::ggsave(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots/y_z_coord_reference.PNG"),
+                    p3,
+                    width = 500,
+                    height = 500,
+                    units = "px")
+  }
+}
+
+
+#' Subset a spatial dataset by coordinates for analysis
+#'
+#' @param filepath Path to directory within which the atlas structure was generated
+#' @param region The anatomical region with which the analysis is associated
+#' @param spatial.data.name A string, the name of the spatial dataset
+#' @param subset.specs A list with length equal to the number of axises, with 
+#'    each entry a vector of length two, with the first element being the
+#'    minimum value to include and the second being the maximum, or NaN to
+#'    indicate a missing value
+#' @param out.filepath Path to directory to save subset data to, if NULL the 
+#'    expression and coordinates in the atlas directory structure are 
+#'    overwritten
+subset_spatial_data = function(filepath,
+                               region,
+                               spatial.data.name,
+                               subset.specs = list(c(NaN, NaN),
+                                                   c(NaN, NaN),
+                                                   c(NaN, NaN)),
+                               out.filepath = NULL){
+  coords = readRDS(paste0(filepath,"/",  region, "/", region,"_Deconvolution_Output/",spatial.data.name,"/",spatial.data.name,"_coords.RDS"))
+  spatial.data = readRDS(paste0(filepath,"/",  region, "/", region,"_Deconvolution_Output/",spatial.data.name,"/",spatial.data.name,"_exp.RDS"))
+  for(i in 1:ncol(coords)){
+    subset.specs[[i]][is.nan(subset.specs[[i]])] = range(coords[i,])[is.nan(subset.specs[[i]])]
+    coords = coords[coords[i,] >= subset.specs[[i]][1] & coords[i,] <= subset.specs[[i]][2], ]
+  }
+  if(nrow(coords) == 0){
+    stop("No samples selected -- provide a new range of coordinates.")
+  } else {
+    message(paste0("Sample subset to ", nrow(coords), " samples."))
+  }
+  spatial.data = spatial.data[, rownames(coords)]
+  if(!is.null(out.filepath)){
+    saveRDS(spatial.data, paste0(out.filepath, "/", spatial.data.name,"_exp.RDS"))
+    saveRDS(coords, paste0(out.filepath, "/", spatial.data.name,"_coords.RDS"))
+  } else {
+    saveRDS(spatial.data, paste0(filepath,"/",  region, "/", region,"_Deconvolution_Output/",spatial.data.name,"/",spatial.data.name,"_exp.RDS"))
+    saveRDS(coords, paste0(filepath,"/",  region, "/", region,"_Deconvolution_Output/",spatial.data.name,"/",spatial.data.name,"_coords.RDS"))
   }
 }
 
@@ -969,107 +1117,6 @@ analyze_gene_signatures = function(filepath,
   detach("package:ggplot2", unload = TRUE)
 }
 
-#' Generate plots of the provided spatial data on the XY, YZ, and XZ planes in
-#' a reference grid to allow for indexing.
-#'
-#' @param filepath Path to directory within which the atlas structure was generated
-#' @param region A string corresponding to the name of an anatomical region
-#' @param spatial.data.name A string, the name of the spatial dataset
-#' @param display.plots A logical, corresponding with if to display requested plots upon
-#'    generation
-#'
-#' @return nothing
-#'
-#' @import ggplot2, cowplot, viridis
-#'
-#' @export
-#' @examples
-#' \dontrun{
-#'
-#' }
-#'
-
-reference_3d_coordinates = function(filepath,
-                                    region,
-                                    spatial.data.name,
-                                    save.plots = FALSE){
-
-  library(ggplot2)
-  library(cowplot)
-  coords = readRDS(paste0(filepath,"/",  region, "/", region,"_Deconvolution_Output/",spatial.data.name,"/",spatial.data.name,"_coords.RDS"))
-
-  minmax = apply(coords, MARGIN = 2, function(x){range(x)})
-  ranges = apply(minmax, MARGIN = 2, function(y){return(y[2]-y[1])})
-
-  p1 = ggplot(coords, aes_string(x = colnames(coords)[1], y = colnames(coords)[2]))+
-    geom_tile() +
-    coord_fixed(ratio = 1)+
-    geom_tile(aes(alpha = .1)) +
-    coord_fixed(ratio = 1) +
-    theme_minimal_grid() +
-    scale_y_continuous(minor_breaks = seq(minmax[1,2] , minmax[2,2], 1), breaks = seq(minmax[1,2] , minmax[2,2], 5)) +
-    scale_x_continuous(minor_breaks = seq(minmax[1,1] , minmax[2,1], 1), breaks = seq(minmax[1,1] , minmax[2,1], 5)) +
-    xlab(colnames(coords)[1]) +
-    ylab(colnames(coords)[2]) +
-    ggtitle(paste0("X-Y silhouette of ",region))+
-    theme(legend.position="none",
-          text = ggplot2::element_text(size = 5),
-          axis.text = ggplot2::element_text(size = 4),
-          plot.title = element_text(size=6))
-  p2 = ggplot(coords, aes_string(x = colnames(coords)[1], y = colnames(coords)[3])) +
-    geom_tile() +
-    coord_fixed(ratio = 1)+
-    geom_tile(aes(alpha = .1)) +
-    coord_fixed(ratio = 1) +
-    theme_minimal_grid() +
-    scale_y_continuous(minor_breaks = seq(minmax[1,3] , minmax[2,3], 1), breaks = seq(minmax[1,3] , minmax[2,3], 5)) +
-    scale_x_continuous(minor_breaks = seq(minmax[1,1] , minmax[2,1], 1), breaks = seq(minmax[1,1] , minmax[2,1], 5)) +
-    xlab(colnames(coords)[1]) +
-    ylab(colnames(coords)[3]) +
-    ggtitle(paste0("X-Z silhouette of ",region))+
-    theme(legend.position="none",
-          text = ggplot2::element_text(size = 5),
-          axis.text = ggplot2::element_text(size = 4),
-          plot.title = element_text(size=6))
-  p3 = ggplot(coords, aes_string(x = colnames(coords)[2], y = colnames(coords)[3])) +
-    geom_tile() +
-    coord_fixed(ratio = 1)+
-    geom_tile(aes(alpha = .1)) +
-    coord_fixed(ratio = 1) +
-    theme_minimal_grid() +
-    scale_y_continuous(minor_breaks = seq(minmax[1,3] , minmax[2,3], 1), breaks = seq(minmax[1,3] , minmax[2,3], 5)) +
-    scale_x_continuous(minor_breaks = seq(minmax[1,2] , minmax[2,2], 1), breaks = seq(minmax[1,2] , minmax[2,2], 5)) +
-    xlab(colnames(coords)[2]) +
-    ylab(colnames(coords)[3]) +
-    ggtitle(paste0("Y-Z silhouette of ",region))+
-    theme(legend.position="none",
-          text = ggplot2::element_text(size = 5),
-          axis.text = ggplot2::element_text(size = 4),
-          plot.title = element_text(size=6))
-  print(p1)
-  print(p2)
-  print(p3)
-  if(save.plots){
-    if(!dir.exists(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots"))){
-      dir.create(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/plots"))
-    }
-    ggplot2::ggsave(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots/x_y_coord_reference.PNG"),
-                    p1,
-                    width = 500,
-                    height = 500,
-                    units = "px")
-    ggplot2::ggsave(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots/x_z_coord_reference.PNG"),
-                    p2,
-                    width = 500,
-                    height = 500,
-                    units = "px")
-    ggplot2::ggsave(paste0(filepath,"/",  region,"/", region,"_Deconvolution_Output/",spatial.data.name,"/plots/y_z_coord_reference.PNG"),
-                    p3,
-                    width = 500,
-                    height = 500,
-                    units = "px")
-  }
-}
 
 #' Generate 2D plots of cell type and gene distribution, given a plane.
 #'
